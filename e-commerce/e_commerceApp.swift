fbee28f8837a37210f00c8a3be1cd675aebd5aad
assignment code <mohamedzaher.dev@gmail.com>
2021-12-10 16:07:01 +0200

    Initial Commit

---
+++ e-commerce/e_commerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_commerceApp.swift
+//  e-commerce
+//
+//  Created by Mohamed Zaher on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct e_commerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
